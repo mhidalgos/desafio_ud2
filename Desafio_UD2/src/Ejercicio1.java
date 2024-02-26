017e61d2ad0c26a8312d98b9c1ee4185b0fa2872
mariajose <mariajose>
2024-02-26 22:31:04 +0100

    MI PRIMER COMMIT

---
+++ Desafio_UD2/src/Ejercicio1.java
@@ -0,0 +1,14 @@
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Escribe un programa en Java que muestre los números del 10 al 1, en ese orden. 
+		// Debes utilizar una estructura de tipo while ó do-while.
+		
+		
+		
+
+	}
+
+}
