4d0de801c206f81df537d9cae322aa3c3e4e6e54
mariajose <mariajose@JMCALLE-LAPTOP>
2024-02-27 23:29:13 +0100

    Ejercicio 2 finalizado

--- Desafio_UD2/src/Ejercicio1.java
+++ Desafio_UD2/src/Ejercicio1.java
@@ -6,7 +6,8 @@ public class Ejercicio1 {
 		// Escribe un programa en Java que muestre los números del 10 al 1, en ese orden. 
 		// Debes utilizar una estructura de tipo while ó do-while.
 		
-		System.out.print("Hola");
+		
+		;
 		
 
 	}
--- Desafio_UD2/src/Ejercicio2.java
+++ Desafio_UD2/src/Ejercicio2.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Escribe un programa en Java que pida un número por teclado hasta que éste sea positivo. 
+		//A continuación, muestra los primeros 20 números sucesivos a dicho número.
+		int num, contador;
+		Scanner sc = new Scanner (System.in);
+		do {
+			System.out.println ("Introduce un número: ");
+			num = sc.nextInt();
+		}while (num<0);
+		sc.close();
+		contador = 1;
+		while (contador<20) {
+			System.out.println (num + contador);
+			contador++;
+		}
+		
+	
+			
+			
+		
 	}
 
 }
