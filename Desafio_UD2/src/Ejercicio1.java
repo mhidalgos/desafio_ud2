88238c9affe90d8cdc8bc4ff62e58338547bf731
mariajose <mariajose@JMCALLE-LAPTOP>
2024-02-27 23:30:27 +0100

    Ejercicio 1 finalizado

--- Desafio_UD2/src/Ejercicio1.java
+++ Desafio_UD2/src/Ejercicio1.java
@@ -5,11 +5,16 @@ public class Ejercicio1 {
 		// TODO Auto-generated method stub
 		// Escribe un programa en Java que muestre los números del 10 al 1, en ese orden. 
 		// Debes utilizar una estructura de tipo while ó do-while.
-		
-		
-		;
-		
-
+	
+		int i = 10;
+		while (i>=1) {
+		System.out.println (i);
+		i--;
+			
+		}
 	}
 
 }
+		
+		
+	
